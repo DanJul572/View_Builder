647dcd7898c0fb136d0c2113a6a1a703f9e744e8
Dandi <dandi.juliandi@nawadata.id>
2025-02-24 20:29:39 +0700

    refactor eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,9 @@
 import { fixupConfigRules } from '@eslint/compat';
-import globals from 'globals';
 import js from '@eslint/js';
-import react from 'eslint-plugin-react/configs/recommended.js';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactJsx from 'eslint-plugin-react/configs/jsx-runtime.js';
+import react from 'eslint-plugin-react/configs/recommended.js';
+import globals from 'globals';
 
 export default [
   { languageOptions: { globals: globals.browser } },
